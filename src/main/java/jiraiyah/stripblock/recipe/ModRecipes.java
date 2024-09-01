e849873ab4a1456aeb9a6e3fafffc82c449a832c
khodakarami <khodakarami_dr@outlook.com>
2024-09-01 16:24:18 +0330

    Working on Update to 1.21.1
    Added new Recipe class and Serializer

--- src/main/java/jiraiyah/stripblock/recipe/ModRecipes.java
+++ src/main/java/jiraiyah/stripblock/recipe/ModRecipes.java
@@ -1,19 +1,36 @@
 package jiraiyah.stripblock.recipe;
 
+import net.minecraft.recipe.Recipe;
+import net.minecraft.recipe.RecipeSerializer;
+import net.minecraft.recipe.RecipeType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
 import static jiraiyah.stripblock.Reference.*;
 
 public class ModRecipes
 {
+    public static final RecipeType<StripRecipe> WOOD_STRIP_TYPE =
+            Registry.register(Registries.RECIPE_TYPE, identifier("wood_stripping"),
+                              new RecipeType<StripRecipe>()
+                                    {
+                                        public String toString()
+                                        {
+                                            return "wood_stripping";
+                                        }
+                                    });
+
+    public static final RecipeSerializer<StripRecipe> WOOD_STRIP_SERIALIZER =
+            Registry.register(Registries.RECIPE_SERIALIZER, "",
+                              new StripSerializer(StripRecipe::new, 0));
+
     public static void register()
     {
         logN(">>> Registering Recipes for : " + ModID);
 
         Registry.register(Registries.RECIPE_SERIALIZER, identifier(StripperBlockRecipe.Serializer.ID),
                 StripperBlockRecipe.Serializer.INSTANCE);
-        Registry.register(Registries.RECIPE_TYPE, identifier(StripperBlockRecipe.Type.ID),
-                StripperBlockRecipe.Type.INSTANCE);
+
     }
 }
---
+++ src/main/java/jiraiyah/stripblock/recipe/StripRecipe.java
@@ -0,0 +1,90 @@
+package jiraiyah.stripblock.recipe;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.recipe.*;
+import net.minecraft.recipe.book.CookingRecipeCategory;
+import net.minecraft.recipe.book.CraftingRecipeCategory;
+import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.recipe.input.SingleStackRecipeInput;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.world.World;
+
+public class StripRecipe implements Recipe<SingleStackRecipeInput>
+{
+    protected final CraftingRecipeCategory category;
+    protected final String group;
+    protected final Ingredient ingredient;
+    protected final ItemStack result;
+    protected final float experience;
+    protected final int processTime;
+
+    public StripRecipe(String group, CraftingRecipeCategory category, Ingredient ingredient, ItemStack result, float experience, int processTime)
+    {
+        this.category = category;
+        this.group = group;
+        this.ingredient = ingredient;
+        this.result = result;
+        this.experience = experience;
+        this.processTime = processTime;
+    }
+
+    @Override
+    public boolean matches(SingleStackRecipeInput input, World world)
+    {
+        if(world.isClient)
+            return false;
+        return this.ingredient.test(input.item());
+    }
+
+    @Override
+    public ItemStack craft(SingleStackRecipeInput input, RegistryWrapper.WrapperLookup lookup)
+    {
+        return this.result.copy();
+    }
+
+    @Override
+    public boolean fits(int width, int height)
+    {
+        return true;
+    }
+
+    @Override
+    public ItemStack getResult(RegistryWrapper.WrapperLookup registriesLookup)
+    {
+        return this.result;
+    }
+
+    @Override
+    public DefaultedList<Ingredient> getIngredients()
+    {
+        DefaultedList<Ingredient> defaultedList = DefaultedList.of();
+        defaultedList.add(this.ingredient);
+        return defaultedList;
+    }
+
+    @Override
+    public String getGroup() {
+        return this.group;
+    }
+
+    @Override
+    public RecipeSerializer<?> getSerializer()
+    {
+        return ModRecipes.WOOD_STRIP_SERIALIZER;
+    }
+
+    @Override
+    public RecipeType<?> getType()
+    {
+        return ModRecipes.WOOD_STRIP_TYPE;
+    }
+
+    public CraftingRecipeCategory getCategory() {
+        return this.category;
+    }
+
+    public interface RecipeFactory<T extends StripRecipe> {
+        T create(String group, CraftingRecipeCategory category, Ingredient ingredient, ItemStack result, float experience, int processTime);
+    }
+}
